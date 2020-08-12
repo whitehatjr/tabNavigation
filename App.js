ec54bb098d26ea0a41d7d62fbafd64ac13ba8a81
vishalgaddam873 <vishal18@navgurukul.org>
2020-08-12 17:42:06 +0530

    Bug fixed

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import TransactionScreen from "./screens/BookTransactionScreen";
+import SearchScreen from "./screens/SearchScreen";
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
 }
+
+const TabNavigator = createBottomTabNavigator({
+  Transaction: { screen: TransactionScreen },
+  Search: { screen: SearchScreen }
+});
+
+const AppContainer = createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
